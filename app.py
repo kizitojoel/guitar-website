cb62191b4e3979b31ca779a9898e3cab78f8997c
Joel Kizito <kizitojoel16@gmail.com>
2023-02-08 23:57:23 +0300

    Complete log in and log out functions
    Along with sesison management, this patch fixes the logging in and
    logging out of a user

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
-from helpers import login_required
+from helpers import login_required, apology
 
 
 #Configure application
@@ -23,11 +23,18 @@ Session(app)
 # CS50 Library to use SQLite
 db = SQL("sqlite:///guitar.db")
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/")
 @login_required
-def hello():
-    return "Hello World"
+def index():
+    return render_template("index.html")
 
 # Route for the register, including SQL support for storing usernames and passwords
 @app.route("/register", methods = ["GET", "POST"])
@@ -35,7 +42,26 @@ def register():
     if request.method == "GET":
         return render_template("register.html")
     else:
-        pass
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("password-confirm")
+
+        # Check if the passwords match each other
+        if password != confirm:
+            return apology("Passwords don't match")
+
+        # Check if username is already taken
+        existingUsers = db.execute("SELECT * FROM users WHERE username = ?;", username)
+        if existingUsers:
+            return apology("Username already taken")
+
+        #Add the username to the database and the password hash
+        password_hash = generate_password_hash(password)
+        db.execute("INSERT INTO users(username, hash) VALUES (?, ?);", username, password_hash)
+
+        #Alert registration successful and redirect to the login page
+        flash("You have been successfully registered", "info")
+        return render_template("login.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -43,6 +69,33 @@ def login():
     """Log our guitar user in"""
 
     session.clear()
-    if request.method == "GET":
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        #Check if the password matches the password hash
+        # If yes, log the user in and redirect them to the homepage
+        rows = db.execute("SELECT * FROM users WHERE username = ?;", username)
+        print(rows)
+
+        #Check if there is such a user
+        print("This thing works")
+        if len(rows) != 1:
+            print("No user")
+            return apology("No such user exists")
+            
+
+        password_hash = rows[0]["hash"]
+        if check_password_hash(password_hash, password):
+            session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+    else:
         return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
 
--- helpers.py
+++ helpers.py
@@ -5,15 +5,18 @@ import urllib.parse
 from flask import redirect, render_template, request, session
 from functools import wraps
 
-def login_required(f):
-    """
-    Decorate routes to require login.
+def apology(message, code=400):
+    def remove_special(s):
+        for special, new in [(" ", "-"), ("-", "--"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(special, new)
+        return s
+    return render_template("apology.html", top=code, bottom=remove_special(message)), code
 
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
-    """
+def login_required(f):
     @wraps(f)
-    def decorated_function(*args, **kwargs):
+    def decorate_function(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
         return f(*args, **kwargs)
-    return decorated_function
+    return decorate_function
